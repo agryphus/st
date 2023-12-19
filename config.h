a16ba503fbfc5d11c08dc3275196eb9744197332
agryphus <agryphus@gmail.com>
2023-12-19 16:11:23 -0500

    Simpler anysize

--- config.h
+++ config.h
@@ -174,7 +174,7 @@ static MouseShortcut mshortcuts[] = {
     
     // Scroll
     { XK_NO_MOD,            Button4, kscrollup,      {.i = 1},      0, S_PRI},
-	{ XK_NO_MOD,            Button5, kscrolldown,    {.i = 1},      0, S_PRI},
+    { XK_NO_MOD,            Button5, kscrolldown,    {.i = 1},      0, S_PRI},
 
     // { ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
     // { XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
@@ -202,9 +202,12 @@ static Shortcut shortcuts[] = {
     { TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 
     // Scroll
-   	{ ShiftMask,            XK_Page_Up,     kscrollup,       {.i = -1}, S_PRI },
-	{ ShiftMask,            XK_Page_Down,   kscrolldown,     {.i = -1}, S_PRI },
+    { ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1}, S_PRI },
+    { ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1}, S_PRI },
 
+    // Transparency
+    { MODKEY,               XK_s,           changealpha,    {.f = -0.05} },
+    { MODKEY,               XK_a,           changealpha,    {.f = +0.05} },
 };
 
 /*
